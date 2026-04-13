1420b5e8ef4bced3ab20c25143a77a777b5c7b74
hydra-vignesh <vigneshprakash2510@gmail.com>
2026-04-13 13:34:45 +0530

    UC16:Sort Passenger Bogies by Capacity (Bubble Sort – Algorithm Intro)

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,62 +1,31 @@
 public class TrainConsistManagementApp {
 
-    // Custom Runtime Exception
-    static class CargoSafetyException extends RuntimeException {
-        public CargoSafetyException(String message) {
-            super(message);
-        }
-    }
-
-    // Goods Bogie model
-    static class GoodsBogie {
-        String type;
-        String cargo;
-
-        GoodsBogie(String type) {
-            this.type = type;
-        }
-
-        void assignCargo(String cargo) {
-            try {
-                // Unsafe condition
-                if (type.equals("Rectangular") && cargo.equals("Petroleum")) {
-                    throw new CargoSafetyException("Unsafe: Petroleum cannot be assigned to Rectangular bogie");
-                }
-
-                this.cargo = cargo;
-                System.out.println("Cargo assigned successfully: " + type + " -> " + cargo);
-
-            } catch (CargoSafetyException e) {
-                System.out.println("Error: " + e.getMessage());
-
-            } finally {
-                System.out.println("Assignment attempt completed for " + type);
-            }
-        }
-
-        public String toString() {
-            return type + " (" + cargo + ")";
-        }
-    }
-
     public static void main(String[] args) {
 
         System.out.println("==================================");
-        System.out.println("UC15 - Safe Cargo Assignment");
+        System.out.println("UC16 - Sort Passenger Bogies by Capacity");
         System.out.println("==================================");
 
-        GoodsBogie b1 = new GoodsBogie("Cylindrical");
-        GoodsBogie b2 = new GoodsBogie("Rectangular");
+        int[] capacities = {72, 56, 24, 70, 60};
 
-        // Safe assignment
-        b1.assignCargo("Petroleum");
+        System.out.print("Before Sorting: ");
+        for (int c : capacities) {
+            System.out.print(c + " ");
+        }
 
-        // Unsafe assignment
-        b2.assignCargo("Petroleum");
+        for (int i = 0; i < capacities.length - 1; i++) {
+            for (int j = 0; j < capacities.length - i - 1; j++) {
+                if (capacities[j] > capacities[j + 1]) {
+                    int temp = capacities[j];
+                    capacities[j] = capacities[j + 1];
+                    capacities[j + 1] = temp;
+                }
+            }
+        }
 
-        // Program continues
-        System.out.println("Final Bogies:");
-        System.out.println(b1);
-        System.out.println(b2);
+        System.out.print("\nAfter Sorting: ");
+        for (int c : capacities) {
+            System.out.print(c + " ");
+        }
     }
 }
