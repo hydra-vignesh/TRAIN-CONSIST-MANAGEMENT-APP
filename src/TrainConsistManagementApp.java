0558923255506e5a9023da054541a92cfc923607
hydra-vignesh <vigneshprakash2510@gmail.com>
2026-04-13 13:37:33 +0530

    sort bogie names using arrays.sort()

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,31 +1,25 @@
+import java.util.Arrays;
+
 public class TrainConsistManagementApp {
 
     public static void main(String[] args) {
 
         System.out.println("==================================");
-        System.out.println("UC16 - Sort Passenger Bogies by Capacity");
+        System.out.println("UC17 - Sort Bogie Names Using Arrays.sort()");
         System.out.println("==================================");
 
-        int[] capacities = {72, 56, 24, 70, 60};
+        String[] bogieNames = {
+                "Sleeper",
+                "AC Chair",
+                "First Class",
+                "General",
+                "Luxury"
+        };
 
-        System.out.print("Before Sorting: ");
-        for (int c : capacities) {
-            System.out.print(c + " ");
-        }
+        System.out.println("Before Sorting: " + Arrays.toString(bogieNames));
 
-        for (int i = 0; i < capacities.length - 1; i++) {
-            for (int j = 0; j < capacities.length - i - 1; j++) {
-                if (capacities[j] > capacities[j + 1]) {
-                    int temp = capacities[j];
-                    capacities[j] = capacities[j + 1];
-                    capacities[j + 1] = temp;
-                }
-            }
-        }
+        Arrays.sort(bogieNames);
 
-        System.out.print("\nAfter Sorting: ");
-        for (int c : capacities) {
-            System.out.print(c + " ");
-        }
+        System.out.println("After Sorting: " + Arrays.toString(bogieNames));
     }
 }
