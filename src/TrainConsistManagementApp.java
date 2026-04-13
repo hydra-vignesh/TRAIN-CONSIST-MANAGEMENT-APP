16ef9656aa30df470f05b524b33636c5fd085771
hydra-vignesh <vigneshprakash2510@gmail.com>
2026-04-13 13:40:46 +0530

    UC19:Binary Search for Bogie ID (Optimized Searching)

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -3,31 +3,36 @@ public class TrainConsistManagementApp {
     public static void main(String[] args) {
 
         System.out.println("==================================");
-        System.out.println("UC18 - Linear Search for Bogie ID");
+        System.out.println("UC19 - Binary Search for Bogie ID");
         System.out.println("==================================");
 
+        // Sorted array (VERY IMPORTANT for Binary Search)
         String[] bogieIds = {"BG101", "BG205", "BG309", "BG412", "BG550"};
 
         String searchId = "BG309";
 
-        System.out.println("Available Bogie IDs:");
-        for (String id : bogieIds) {
-            System.out.print(id + " ");
-        }
-
+        int low = 0;
+        int high = bogieIds.length - 1;
         boolean found = false;
 
-        for (String id : bogieIds) {
-            if (id.equals(searchId)) {
+        while (low <= high) {
+
+            int mid = (low + high) / 2;
+
+            int result = searchId.compareTo(bogieIds[mid]);
+
+            if (result == 0) {
                 found = true;
                 break;
+            } else if (result > 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
 
-        System.out.println();
-
         if (found) {
-            System.out.println("Bogie ID " + searchId + " found in the train.");
+            System.out.println("Bogie ID " + searchId + " found using Binary Search.");
         } else {
             System.out.println("Bogie ID " + searchId + " not found.");
         }
